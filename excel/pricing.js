52b1f338faeb1757670437fe757f96e745c098e7
Walid <hello@walidvb.com>
2020-10-18 16:15:51 +0200

    fix price min

--- excel/pricing.js
+++ excel/pricing.js
@@ -1,5 +1,5 @@
 module.exports = {
-  computeStepPrice: (step) => step.quantity ? Math.max(step.unit_price, step.price*step.quantity) : 0,
+  computeStepPrice: (step) => step.quantity ? Math.max(step.min_price, step.price*step.quantity) : 0,
   computeRoomTotal: ({ steps }) => steps.reduce((prev, curr) => prev + computeStepPrice(curr), 0),
   countProjectSteps: ({ version: { rooms } }) => rooms.reduce((prev, curr) => prev + curr.steps.length, 0),
   computeProjectTotal: ({ version: { rooms } }) => rooms.reduce((prev, r) => prev + computeRoomTotal(r), 0),
